e449d93e0cb44d42fc0a7e339f0295bf863d96b8
a-trefilova <atrefilova97@icloud.com>
2020-06-10 12:36:00 +0300

    Initial Commit

---
+++ NewsfeedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsfeedApp
+//
+//  Created by Константин Сабицкий on 10.06.2020.
+//  Copyright © 2020 Константин Сабицкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
